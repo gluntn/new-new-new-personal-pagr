fb5361bf524dd1da2526dfaf1a6e20a87605f2e1
William <wostensen96@gmail.com>
2017-12-17 23:02:41 +0100

    Made several changes.

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,5 @@
-const menuButton = document.getElementById("mobile");
-const slideMenu  = document.getElementById("mobile-menu")
-const feedbackButton = document.getElementById("feedback");
+const menuButton     = document.getElementById("mobile");
+const slideMenu      = document.getElementById("mobile-menu")
 
 var switchClasses = function(el, cOne, cTwo) {
 	  if(el.classList) {
@@ -17,13 +16,9 @@ menuButton.addEventListener('click', (e) => {
   if(slideMenu.classList.contains('super-hide')) {
     switchClasses(slideMenu, 'super-hide', 'super-show');
     menuButton.classList.add("close");
-  } 
+  }
   else if(slideMenu.classList.contains('super-show')) {
     switchClasses(slideMenu, 'super-show', 'super-hide');
     menuButton.classList.remove("close");
   }
-});
-
-feedbackButton.addEventListener('click', (e) => {
-  alert("Sorry! Not implemented yet!");
 });
