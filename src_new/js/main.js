9b2ded311ecdafd1a1a34e4ee147b8c55680ef28
William <wostensen96@gmail.com>
2018-01-08 11:11:52 +0100

    Fixed mobile menu, media queries for bigger screens

--- src_new/js/main.js
+++ src_new/js/main.js
@@ -1,6 +1,14 @@
 const openMenu   = document.getElementById("open-menu")
 const navigation = document.getElementById("navigation")
+const overlay    = document.getElementById("overlay")
 
 openMenu.addEventListener('click', (e) => {
   navigation.classList.toggle("show");
+  if(navigation.classList.contains("show")) { overlay.classList.remove("hidden") }
+  else { overlay.classList.add("hidden"); }
+});
+
+overlay.addEventListener('click', (e) => {
+  if(navigation.classList.contains("show")) { navigation.classList.remove("show"); }
+  overlay.classList.toggle("hidden");
 });
